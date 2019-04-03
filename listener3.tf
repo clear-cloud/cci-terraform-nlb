7f50c17cb5a2daf315baf1b3215daccdfe7ffc8f
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-04-03 11:02:06 +0000

    Adding optional listeners

--- listener3.tf
+++ listener3.tf
@@ -37,8 +37,14 @@ resource "aws_lb_target_group" "l3_alb_target_group" {
 # Listener2 - Target Group Attachment
 # -------------------
 resource "aws_lb_target_group_attachment" "l3_target_group" {
-  count            = "${length(split(",", var.listener3_target_id))}"
+  count            = "${var.listener3_target_id != "0" ? length(split(",", var.listener3_target_id)) : 0}"
   target_group_arn = "${aws_lb_target_group.l3_alb_target_group.arn}"
   target_id        = "${element(split(",",var.listener3_target_id), count.index)}"
   port             = "${var.listener3_svc_port}"
 }
+
+
+
+
+
+#count            = "${var.listener3_target_id != "0" ? length(split(",", var.listener3_target_id)) : 0}
