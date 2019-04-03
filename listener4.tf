c215472245b6b2dedeb81c77dd06c0fb128453a8
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-04-03 11:33:05 +0000

    4th listener added

--- listener4.tf
+++ listener4.tf
@@ -18,7 +18,7 @@ resource "aws_alb_listener" "l4_alb_listener" {
 # -------------------
 resource "aws_lb_target_group" "l4_alb_target_group" {
   count    = "${var.listener3_alb_listener_port != "" ? 1 : 0}"
-  name     = "${var.name}-${var.environment}-tg-listener4-${var.listener3_svc_port}"
+  name     = "${var.name}-${var.environment}-tg-listener4-${var.listener4_svc_port}"
   port     = "${var.listener4_svc_port}"
   protocol = "${var.listener4_target_group_protocol}"
   vpc_id   = "${var.vpc_id}"
