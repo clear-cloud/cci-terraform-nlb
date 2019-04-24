622174ef00734541ff888027663de0328538afc5
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-04-24 11:12:55 +0000

    optional CNAME record

--- route53CNAME.tf
+++ route53CNAME.tf
@@ -2,10 +2,11 @@
 # Optional friendly CNAME entry
 #
 resource "aws_route53_record" "cname" {
-  count   = "${var.friendly_name ? 1 : 0}"
+  count   = "${length(split(",", var.friendly_name)) != 0 ? length(split(",", var.friendly_name)) : 0}"
   zone_id = "${var.zone_id}"
   name    = "${var.friendly_name}"
   type    = "CNAME"
   ttl     = "300"
   records = ["${aws_lb.alb.dns_name}"]
 }
+
